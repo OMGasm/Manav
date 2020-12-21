94c5e182e907e6d3978c68903f9c8c0954a28955
Tim <OMGasm@users.noreply.github.com>
2020-12-22 01:19:35 +1000

    init

---
+++ Manav/DiscordService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Manav
+{
+    class DiscordService : Microsoft.Extensions.Hosting.BackgroundService
+    {
+        private IConfiguration _config;
+        private DiscordSocketClient _client;
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _client = new();
+            _config = BuildConfig();
+            var services = ConfigureServices();
+            services.GetRequiredService<LogService>();
+            await Task.Delay(-1, stoppingToken);
+        }
+
+        private IServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                .AddLogging()
+                .AddSingleton<LogService>()
+                .AddSingleton(_config)
+                .BuildServiceProvider();
+        }
+
+        private IConfiguration BuildConfig()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json", true)
+                .Build();
+        }
+    }
+}
---
+++ Manav/LogService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace Manav
+{
+    public class LogService
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly ILogger _clientLogger;
+        private readonly ILogger _commandLogger;
+
+        public LogService(DiscordSocketClient client, CommandService commands, ILoggerFactory loggerFactory)
+        {
+            _client = client;
+            _commands = commands;
+            _loggerFactory = ConfigureLogging(loggerFactory);
+            _clientLogger = _loggerFactory.CreateLogger("discord");
+            _commandLogger = _loggerFactory.CreateLogger("commands");
+        }
+
+        private ILoggerFactory ConfigureLogging(ILoggerFactory factory)
+        {
+            return factory;
+        }
+    }
+}
